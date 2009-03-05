64e2cd1f8732a83871a60cf988d7703960ed1245
Aleksandar <aleks.dimitrov@gmail.com>
2009-03-05 13:15:30 +0100

    Added Yi configuration file.

---
+++ yi.hs
@@ -0,0 +1,36 @@
+-- yi.hs
+-- Aleksandar Dimitrov
+
+import Yi
+import Yi.Keymap.Vim (keymap)
+import qualified Yi.Mode.Haskell as Haskell
+import Yi.Style
+import Yi.Hoogle (hoogle, hoogleSearch)
+import Yi.Style.Library
+import Yi.Prelude
+import Prelude ()
+
+defaultVimUiTheme :: Theme
+defaultVimUiTheme = defaultLightTheme  `override` \super self ->
+        super { selectedStyle = modelineFocusStyle self }
+
+myConfigUI :: UIConfig
+myConfigUI = (configUI defaultConfig)  
+	{ configTheme = defaultVimUiTheme
+        , configWindowFill = '~'
+	}
+
+haskellModeKeys = [ ctrlCh 'c' ?>> char 'g' ?>>! Haskell.ghciLoadBuffer
+                  , ctrlCh 'c' ?>> char 'l' ?>>! Haskell.ghciGet
+                  , ctrlCh 'c' ?>> char 'h' ?>>! hoogle
+                  , ctrlCh 'c' ?>> char 's' ?>>! hoogleSearch
+		  ]
+
+myHaskellMode = Haskell.cleverMode { modeKeymap = (choice haskellModeKeys <||) }
+
+main :: IO ()
+main = yi $ defaultConfig 
+	{ configUI = myConfigUI
+        , defaultKm = keymap
+	, modeTable = AnyMode myHaskellMode : modeTable defaultConfig
+	}
