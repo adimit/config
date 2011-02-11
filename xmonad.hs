fbf047b3ccc4f463ebea2d1c01ea284f726858e8
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2011-02-11 19:28:06 +0100

    New style, based on lucius gvim colorscheme.

--- xmonad.hs
+++ xmonad.hs
@@ -17,15 +17,19 @@ import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.Reflect
 import XMonad.Hooks.DynamicLog
 
+import XMonad.Actions.Warp
+import XMonad.Layout.PerWorkspace
+
 import System.Info (os)
 
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
-layouts = layoutHintsToCenter
+layouts = smartBorders
+        . desktopLayoutModifiers
         . mkToggle(NOBORDERS ?? FULL ?? EOT)
         . mkToggle(single MIRROR)
-        . desktopLayoutModifiers
+        . layoutHintsToCenter
         $ tiled ||| reflectHoriz (simpleTabbed *|* Full)
     where tiled = XMonad.Tall 1 (3/100) (1/2)
 
@@ -41,25 +45,40 @@ myKeys conf@(XConfig { modMask = mask, workspaces = ws }) = M.fromList $
             , ((mask,               xK_Return    ), spawnHere $ XMonad.terminal conf)
             , ((mask,               xK_grave     ), toggleWS) ]
             ++ -- GridSelect
-            [ ((mask              , xK_g         ), (goToSelected defaultGSConfig)) ]
+            [ ((mask              , xK_g         ), warpToCentre >> goToSelected  myGSConfig)
+            , ((mask .|. shiftMask, xK_g         ), spawnSelected myGSConfig myGSApps) ]
             ++ -- MultiToggle
             [ ((mask .|. shiftMask, xK_f          ), sendMessage $ Toggle FULL)
             , ((mask .|. shiftMask, xK_m          ), sendMessage $ Toggle MIRROR) ]
 
 workspaceKeys = [ xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0 ]
 
-detectModMask = if os == "darwin"
-                   then mod1Mask
-                   else mod4Mask
+warpToCentre = gets (W.screen . W.current . windowset) >>= \x -> warpToScreen x  0.5 0.5
+
+myGSConfig :: HasColorizer a => GSConfig a
+myGSConfig = defaultGSConfig { gs_cellheight = 35
+                             , gs_cellwidth  = 100
+                             , gs_navigate = gsn }
+    where gsn                 = M.unions [ reset, fpsKeys ]
+          addPair (a,b) (x,y) = (a+x,b+y)
+          fpsKeys = M.map addPair $ M.fromList [ ((0,xK_e), (0,1))
+                                               , ((0,xK_o), (-1,0))
+                                               , ((0,xK_u), (1,0))
+                                               , ((0,xK_period), (0,-1)) ]
+          reset = M.singleton (0,xK_space) (const (0,0))
+
+myGSApps = ["opera", "miro", "eclipse", "gmpc", "gvim"]
+
+detectModMask = if os == "darwin" then mod1Mask else mod4Mask
 
 myConfig = gnomeConfig { terminal   = "urxvt"
-                       , layoutHook = smartBorders layouts
+                       , layoutHook = layouts
                        , modMask    = detectModMask
                        , keys       = \c -> myKeys c `M.union` keys gnomeConfig c
                        , manageHook = composeAll [ manageHook gnomeConfig
                                                  , manageSpawn
                                                  , isFullscreen --> doFullFloat ]
-                       , normalBorderColor  = "#000000"
-                       , focusedBorderColor = "#8899ff" }
+                       , normalBorderColor  = "#202020"
+                       , focusedBorderColor = "#cae682" }
 
 main = xmonad =<< xmobar myConfig
