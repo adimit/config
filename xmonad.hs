d3e0370a46f2b3f6908b4c917f7997f023be4b43
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2011-02-12 21:33:03 +0100

    +TopicSpace, +cleanup for xmonad.hs.

--- xmonad.hs
+++ xmonad.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeSynonymInstances, ExtendedDefaultRules, Rank2Types #-}
 import XMonad
 import XMonad.Config.Desktop (desktopLayoutModifiers)
 import XMonad.Config.Gnome
@@ -16,9 +17,10 @@ import XMonad.Layout.Tabbed
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.Reflect
 import XMonad.Hooks.DynamicLog
+import XMonad.Actions.TopicSpace
 
-import XMonad.Actions.Warp
 import XMonad.Layout.PerWorkspace
+import XMonad.Prompt.Workspace
 
 import System.Info (os)
 
@@ -33,52 +35,93 @@ layouts = smartBorders
         $ tiled ||| reflectHoriz (simpleTabbed *|* Full)
     where tiled = XMonad.Tall 1 (3/100) (1/2)
 
-promptConfig = defaultXPConfig
-        { position          = Top
-        , font              = "xft:Droid Sans Mono:size=8"
-        , promptBorderWidth = 0 }
+promptConfig = defaultXPConfig { position          = Top
+                               , font              = myFont
+                               , bgColor           = myHLBG
+                               , fgColor           = myFG
+                               , fgHLight          = myBG
+                               , bgHLight          = myHL
+                               , promptBorderWidth = 0 }
 
-myKeys conf@(XConfig { modMask = mask, workspaces = ws }) = M.fromList $
-            [ ((mask,               xK_a         ), withFocused $ windows . W.sink)
-            , ((mask .|. shiftMask, xK_Return    ), dwmpromote)
-            , ((mask,               xK_BackSpace ), shellPromptHere promptConfig)
-            , ((mask,               xK_Return    ), spawnHere $ XMonad.terminal conf)
-            , ((mask,               xK_grave     ), toggleWS) ]
-            ++ -- GridSelect
-            [ ((mask              , xK_g         ), warpToCentre >> goToSelected  myGSConfig)
-            , ((mask .|. shiftMask, xK_g         ), spawnSelected myGSConfig myGSApps) ]
-            ++ -- MultiToggle
-            [ ((mask .|. shiftMask, xK_f          ), sendMessage $ Toggle FULL)
-            , ((mask .|. shiftMask, xK_m          ), sendMessage $ Toggle MIRROR) ]
+myFont     = "xft:Droid Sans Mono:size=8"
+myBG       = "#202020"
+myFG       = "#EEEEEE"
+myHL       = "#cae682"
+myHLBG     = "#363946"
+myTerminal = "urxvt"
+myGSApps   = ["opera", "miro", "eclipse", "gmpc", "gvim"]
 
-workspaceKeys = [ xK_1, xK_2, xK_3, xK_4, xK_5, xK_6, xK_7, xK_8, xK_9, xK_0 ]
-
-warpToCentre = gets (W.screen . W.current . windowset) >>= \x -> warpToScreen x  0.5 0.5
+spawnShell       = currentTopicDir myTopicConfig >>= spawnShellIn
+spawnShellIn dir = spawn $ myTerminal ++ " -cd ${HOME}/'" ++ escape' dir ++ "'"
+    where escape' ('\'':xs) = "\\'" ++ escape' xs
+          escape'    (x:xs) = x:escape' xs
+          escape'        [] = []
 
 myGSConfig :: HasColorizer a => GSConfig a
-myGSConfig = defaultGSConfig { gs_cellheight = 35
-                             , gs_cellwidth  = 100
-                             , gs_navigate = gsn }
-    where gsn                 = M.unions [ reset, fpsKeys ]
-          addPair (a,b) (x,y) = (a+x,b+y)
-          fpsKeys = M.map addPair $ M.fromList [ ((0,xK_e), (0,1))
-                                               , ((0,xK_o), (-1,0))
-                                               , ((0,xK_u), (1,0))
-                                               , ((0,xK_period), (0,-1)) ]
-          reset = M.singleton (0,xK_space) (const (0,0))
+myGSConfig = defaultGSConfig { gs_cellheight  = 25
+                             , gs_cellwidth   = 100
+                             , gs_navigate    = M.unions [ reset, fpsKeys ]
+                             , gs_font        = myFont
+                             , gs_cellpadding = 4 }
+    where (a,b) <+> (x,y) = (a+x,b+y)
+          reset   = M.singleton (0,xK_space) (const (0,0))
+          fpsKeys = M.map (<+>) $ M.fromList
+                                [ ((0,xK_e),      (0,1))
+                                , ((0,xK_o),      (-1,0))
+                                , ((0,xK_u),      (1,0))
+                                , ((0,xK_period), (0,-1)) ]
 
-myGSApps = ["opera", "miro", "eclipse", "gmpc", "gvim"]
+myKeys conf@(XConfig { modMask = mask, workspaces = ws }) = M.fromList $
+            [ ((mask,               xK_a        ), withFocused $ windows . W.sink)
+            , ((mask .|. shiftMask, xK_Return   ), dwmpromote)
+            , ((mask,               xK_BackSpace), shellPromptHere promptConfig)
+            , ((mask,               xK_grave    ), toggleWS) ]
+            ++ -- GridSelect
+            [ ((mask              , xK_g        ), goToSelected myGSConfig)
+            , ((mask .|. shiftMask, xK_g        ), spawnSelected myGSConfig myGSApps) ]
+            ++ -- MultiToggle
+            [ ((mask .|. shiftMask, xK_f        ), sendMessage $ Toggle FULL)
+            , ((mask .|. shiftMask, xK_m        ), sendMessage $ Toggle MIRROR) ]
+            ++ -- TopicSpace
+            [ ((mask              , xK_t        ), prompt (switchTopic myTopicConfig))
+            , ((mask .|. shiftMask, xK_t        ), prompt $ windows . W.shift)
+            , ((mask              , xK_Return   ), spawnShell)
+            , ((mask              , xK_d        ), runSelectedAction myGSConfig topicGrid) ]
+            where prompt    = workspacePrompt promptConfig
+                  topicGrid = map (\t -> (t, windows $ W.greedyView t)) myTopics
 
-detectModMask = if os == "darwin" then mod1Mask else mod4Mask
+myTopics :: [Topic]
+myTopics = [ "dash", "web", "comm", "werti", "eclipse", "logs", "test", "admin", "music"
+           , "podc", "read", "yi", "vid", "play", "xmonad", "documents", "torrent", "conf"
+           , "src", "picasa", "srv" ]
 
-myConfig = gnomeConfig { terminal   = "urxvt"
+myTopicConfig :: TopicConfig
+myTopicConfig = TopicConfig
+    { defaultTopicAction = const $ spawnShell >*> 3
+    , defaultTopic       = "dash"
+    , maxTopicHistory    = 10
+    , topicActions = M.fromList [ ("conf"     , spawnShell >> spawnHere ed)
+                                , ("werti"    , spawn ed >> spawnShell) ]
+    , topicDirs    = M.fromList [ ("conf"     , "Documents/Configuration/public")
+                                , ("documents", "Documents")
+                                , ("eclipse"  , src ++ "workspace")
+                                , ("yi"       , src ++ "yi")
+                                , ("werti"    , src ++ "werti-passives")
+                                , ("xmonad"   , src ++ "xmonad")
+                                , ("picasa"   , "Pictures")
+                                , ("srv"      , "local/apache-tomcat-6.0.30")
+                                , ("src"      , src) ] }
+    where (src,ed,web) = ("Documents/src/","gvim","opera")
+
+myConfig = gnomeConfig { terminal   = myTerminal
                        , layoutHook = layouts
-                       , modMask    = detectModMask
+                       , modMask    = if os == "darwin" then mod1Mask else mod4Mask
                        , keys       = \c -> myKeys c `M.union` keys gnomeConfig c
+                       , workspaces = myTopics
                        , manageHook = composeAll [ manageHook gnomeConfig
                                                  , manageSpawn
                                                  , isFullscreen --> doFullFloat ]
-                       , normalBorderColor  = "#202020"
-                       , focusedBorderColor = "#cae682" }
+                       , normalBorderColor  = myBG
+                       , focusedBorderColor = myHL}
 
 main = xmonad =<< xmobar myConfig
