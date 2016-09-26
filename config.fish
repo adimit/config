20a34e0f52c324e205e0fb184daceef119b04c91
Aleksandar Dimitrov <aleks.dimitrov@gmail.com>
2016-09-26 22:04:08 +0200

    Work around ctrl-f not working

--- config.fish
+++ config.fish
@@ -51,7 +51,12 @@ function unc
     echo "Removed $oldpwd"
 end
 
-set -g fish_key_bindings fish_vi_key_bindings
+function fish_user_key_bindings
+    fish_vi_mode
+    bind -M insert \cf accept-autosuggestion
+    bind \cf accept-autosuggestion
+end
+
 fish_vi_cursor
 # Suppress default vi-mode indicator.
 function fish_mode_prompt; end
