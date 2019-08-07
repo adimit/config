3bf6af7d382237807557b90003892eb8ac156089
Aleksandar Dimitrov <git@aleks.bg>
2019-08-07 08:17:03 +0200

    Add some modes

--- newmacs/init.el
+++ newmacs/init.el
@@ -13,7 +13,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (counsel-projectile projectile hydra counsel ivy evil-magit magit smartparens use-package evil))))
+    (web-mode tide flycheck company which-key counsel-projectile projectile hydra counsel ivy evil-magit magit smartparens use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
