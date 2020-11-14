7aa03bda553ad3cbddc1976c431427f1586c07b8
Aleksandar Dimitrov <git@aleks.bg>
2020-11-14 10:47:40 +0100

    Only load private definitions after mu4e

--- newmacs/init.el
+++ newmacs/init.el
@@ -13,16 +13,17 @@
       gc-cons-percentage 0.6
       file-name-handler-alist nil)
 
-(let* ((config-emacs-directory "~/config/newmacs")
-       (main-org-file (expand-file-name "main.org" config-emacs-directory))
-       (private-org-file (expand-file-name "private.org" config-emacs-directory)))
-  (if (file-readable-p main-org-file)
-      (org-babel-load-file main-org-file))
-  (if (file-readable-p private-org-file)
-      (org-babel-load-file private-org-file)))
+(defun aleks/load-org-file (name)
+  "Load an org file with NAME from the org directory."
+  (let* ((config-emacs-directory "~/config/newmacs")
+         (org-file (expand-file-name name config-emacs-directory)))
+    (if (file-readable-p org-file)
+        (org-babel-load-file org-file))))
+
+(aleks/load-org-file "main.org")
 
 (add-hook 'emacs-startup-hook
-	  (lambda () (setq gc-cons-threshold 16777216
+          (lambda () (setq gc-cons-threshold 16777216
                            gc-cons-percentage 0.1
                            file-name-handler-alist temporary-file-name-handler-alist)))
 
