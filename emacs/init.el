3410f769b8ed751f6e9fb9e321e2d53fc44f3377
Aleksandar Dimitrov <git@aleks.bg>
2021-08-08 16:10:24 +0200

    Add new emacs config (to keep things fresh)

---
+++ emacs/init.el
@@ -0,0 +1,51 @@
+(defvar temporary-file-name-handler-alist file-name-handler-alist)
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil)
+
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold 16777216
+                           gc-cons-percentage 0.1
+                           file-name-handler-alist temporary-file-name-handler-alist)))
+
+; load straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+; fetch the development version of org-mode before evaluating literate configuration files
+(straight-use-package
+ '(org :type git :repo "https://code.orgmode.org/bzg/org-mode.git"))
+
+; load literate configuration files
+(defun aleks/load-org-file (name)
+  "Load an org file with NAME from the org directory."
+  (let* ((config-emacs-directory "~/config/emacs")
+         (org-file (expand-file-name name config-emacs-directory)))
+    (if (file-readable-p org-file)
+        (org-babel-load-file org-file))))
+
+(aleks/load-org-file "main.org")
+(aleks/load-org-file "private.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
