640e2cc8933355d0b1aaaaa8e1e784bb05104a8a
Aleksandar Dimitrov <git@aleks.bg>
2022-09-01 00:03:59 +0200

    Increase heap

--- emacs/init.el
+++ emacs/init.el
@@ -4,7 +4,7 @@
       file-name-handler-alist nil)
 
 (add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold 16777216
+          (lambda () (setq gc-cons-threshold 100000000
                            gc-cons-percentage 0.1
                            file-name-handler-alist temporary-file-name-handler-alist)))
 
